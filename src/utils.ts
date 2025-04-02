ca9642e56207b23df18549711ee7332b46bad792
azkal279 <clarkrizon@yahoo.com>
2025-04-02 21:26:34 +0000

    Update dependencies - improving performance

--- src/utils.ts
+++ src/utils.ts
@@ -15,7 +15,7 @@ export async function checkForSwingWorkspace() {
   switch (config.get("launchBehavior")) {
     case "openSwing": {
       if (workspace.workspaceFolders) {
-        const files = await workspace.findFiles(SWING_FILE);
+        let files = await workspace.findFiles(SWING_FILE);
         if (files.length > 0) {
           openSwing(workspace.workspaceFolders[0].uri);
         } else if (config.get("rootDirectory")) {
@@ -32,12 +32,12 @@ export async function checkForSwingWorkspace() {
 }
 
 export async function getFileContents(swingUri: Uri, file: string) {
-  const uri = Uri.joinPath(swingUri, file);
+  let uri = Uri.joinPath(swingUri, file);
   return getUriContents(uri);
 }
 
 export async function getUriContents(uri: Uri) {
-  const contents = await workspace.fs.readFile(uri);
+  let contents = await workspace.fs.readFile(uri);
   return byteArrayToString(contents);
 }
 
